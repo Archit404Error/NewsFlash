5c04718969e7a7703cf068e505e7f7953975c076
Archit404Error <4architmehta@gmail.com>
2021-06-17 23:34:12 -0400

    Implemented partial cache bugfix

--- main.py
+++ main.py
@@ -19,18 +19,22 @@ def summaries() -> str:
     #Store user topic from homepage post request
     topic = request.form['topic']
     cache = open('cache.json', 'r+')
+    cached_list = json.load(cache)
 
     if topic in cache.read():
-        cache = open('cache.json', 'r+')
-        converted_data = json.load(cache)
-        sources = converted_data[1]
-        outlet_summaries = converted_data[2]
-        biases = converted_data[3]
-        article_links = converted_data[4]
+        for stored in cached_list:
+            if stored[0] == topic or topic in stored[2].values():
+                sources = stored[1]
+                outlet_summaries = stored[2]
+                biases = stored[3]
+                article_links = stored[4]
     else:
         sources, outlet_summaries, biases, article_links = collect_news(topic)
         json_data = [topic, sources, outlet_summaries, biases, article_links]
-        json.dump(json_data, cache)
+        cache.truncate(0)
+        cached_list.append(json_data)
+        json.dump(cached_list, cache)
+        # Fix whitespace issue
 
     return render_template("summaries.html", topic = topic, sources = sources,
     outlet_summaries = outlet_summaries, biases = biases, article_links = article_links)
