91f6f8b67f20c39b9bd3ace272bfab07295c4b8e
Archit404Error <4architmehta@gmail.com>
2021-06-21 19:18:34 -0400

    Connected API to web client

--- cache_handler.py
+++ cache_handler.py
@@ -6,27 +6,36 @@ def cache_query(cache_path, topic):
     try:
         cached_list = json.load(cache)
     except:
+        #Occurs if we fail to load cache json(only occurs when cache is empty)
         cached_list = []
+    #Reopen cache file so we can re-read from it
     cache = open(cache_path, 'r+')
     if topic in cache.read():
         for stored in cached_list:
+            #Account for keywords by searching for topic in description
             if stored[0] == topic or topic in stored[2].values():
+                #Get cached values to decrease query time
                 sources = stored[1]
                 outlet_summaries = stored[2]
                 biases = stored[3]
                 article_links = stored[4]
+                #Increment times queried value
                 times_queried = stored[5] + 1
+                #Reset the value of this object and add it to json file
                 cache.seek(0)
                 cached_list[cached_list.index(stored)] = [topic, sources, outlet_summaries, biases, article_links, times_queried]
                 json.dump(cached_list, cache)
                 cache.truncate()
     else:
+        #Get values from collect_news function
         sources, outlet_summaries, biases, article_links = collect_news(topic)
         times_queried = 1
+        #Create json data, append it to cached_list, and dump into file
         json_data = [topic, sources, outlet_summaries, biases, article_links, times_queried]
         cache.seek(0)
         cached_list.append(json_data)
         json.dump(cached_list, cache)
         cache.truncate()
 
+    #Send back collected values necessary to displaying articles
     return sources, outlet_summaries, biases, article_links
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, render_template, jsonify
 from cache_handler import cache_query
 import json
+import requests
 
 #TODO: Reset cache daily
 #TODO: Replace trending page using times_queried in json data
@@ -17,7 +18,17 @@ def index() -> str:
 def summaries() -> str:
     #Store user topic from homepage post request
     topic = request.form['topic']
-    sources, outlet_summaries, biases, article_links = cache_query('cache.json', topic)
+    topic = topic.replace(" ", "+")
+
+    url = "http://localhost:5000/api?{}"
+    api_res = requests.get(url.format(topic))
+    res_json = api_res.json()
+
+    topic = res_json["topic"]
+    sources = res_json["sources"]
+    outlet_summaries = res_json["summaries"]
+    biases = res_json["biases"]
+    article_links = res_json["articles"]
 
     return render_template("summaries.html", topic = topic, sources = sources,
     outlet_summaries = outlet_summaries, biases = biases, article_links = article_links)
@@ -35,7 +46,7 @@ def apiRes() -> str:
         topic = item
     sources, outlet_summaries, biases, article_links = cache_query('cache.json', topic)
 
-    return jsonify(topic = topic, articles = article_links, summaries = outlet_summaries)
+    return jsonify(topic = topic, articles = article_links, summaries = outlet_summaries, sources = sources, biases = biases)
 
 if __name__ == "__main__":
     app.run(debug=True)
